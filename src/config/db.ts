9678925481aeffc58523e755ca78a8fa7ef7230c
supakrit-petpon <Supakrit_Petpon@outlook.com>
2025-11-21 21:10:36 +0700

    Adjust config

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,5 +1,8 @@
+import { PrismaPg } from '@prisma/adapter-pg';
 import { PrismaClient } from '@prisma/client';
+import 'dotenv/config';
 
-const db = new PrismaClient();
+const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL! })
+const db = new PrismaClient({ adapter })
 
 export default db;
